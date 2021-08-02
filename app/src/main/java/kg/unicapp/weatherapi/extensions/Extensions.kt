7ab125b73435e0e9c82ae0f2302cb4ae715f303d
agamonch <jnkyrgyz@gmail.com>
2021-08-02 20:45:53 +0300

    changed app name

--- app/src/main/java/kg/unicapp/weatherapi/extensions/Extensions.kt
+++ app/src/main/java/kg/unicapp/weatherapi/extensions/Extensions.kt
@@ -6,7 +6,7 @@ import java.util.*
 fun Long?.format(pattern: String? = "dd/MM/yyyy"): String {
     this?.let {
         val sdf = SimpleDateFormat(pattern, Locale.getDefault())
-        return sdf.format(Date(this *1000+10800000))
+        return sdf.format(Date(this *1000))
     }
     return ""
 
